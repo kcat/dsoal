fa80bf05b46ce2027c7303a567ebdcbdafe5f927
Chris Robinson <chris.kcat@gmail.com>
2023-04-12 16:49:41 -0700

    Implement enumeration

--- src/dsoal.cpp
+++ src/dsoal.cpp
@@ -2,9 +2,13 @@
 
 #include "dsoal.h"
 
+#include <cstdio>
 #include <cstdlib>
+#include <deque>
+#include <devpkey.h>
 #include <dsound.h>
 #include <mmdeviceapi.h>
+#include <mutex>
 
 #include "comptr.h"
 #include "logging.h"
@@ -96,12 +100,45 @@ HRESULT WINAPI DSOAL_DirectSoundFullDuplexCreate(const GUID *captureDevice,
 
 namespace {
 
+class GuidPrinter {
+    char mMsg[64];
+
+public:
+    GuidPrinter(const GUID &guid)
+    {
+        std::snprintf(mMsg, std::size(mMsg), "{%08lx-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x}",
+            DWORD{guid.Data1}, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2],
+            guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
+    }
+    const char *c_str() const { return mMsg; }
+};
+
+class PropVariant {
+    PROPVARIANT mProp;
+
+public:
+    PropVariant() { PropVariantInit(&mProp); }
+    ~PropVariant() { clear(); }
+
+    void clear() { PropVariantClear(&mProp); }
+
+    PROPVARIANT* get() noexcept { return &mProp; }
+
+    PROPVARIANT& operator*() noexcept { return mProp; }
+    const PROPVARIANT& operator*() const noexcept { return mProp; }
+
+    PROPVARIANT* operator->() noexcept { return &mProp; }
+    const PROPVARIANT* operator->() const noexcept { return &mProp; }
+};
+
 struct ComWrapper {
     HRESULT mStatus{};
 
     ComWrapper() { mStatus = CoInitialize(nullptr); }
     ComWrapper(ComWrapper&& rhs) { mStatus = std::exchange(rhs.mStatus, E_FAIL); }
     ComWrapper(const ComWrapper&) = delete;
+    ~ComWrapper() { if(SUCCEEDED(mStatus)) CoUninitialize(); }
+
     ComWrapper& operator=(ComWrapper&& rhs)
     {
         if(SUCCEEDED(mStatus))
@@ -110,12 +147,14 @@ struct ComWrapper {
         return *this;
     }
     ComWrapper& operator=(const ComWrapper&) = delete;
-
-    ~ComWrapper() { if(SUCCEEDED(mStatus)) CoUninitialize(); }
 };
 
+std::mutex gDeviceListMutex;
+std::deque<GUID> gPlaybackDevices;
+std::deque<GUID> gCaptureDevices;
+
 template<typename T>
-HRESULT enumerate_mmdev(EDataFlow flow, T cb)
+HRESULT enumerate_mmdev(const EDataFlow flow, std::deque<GUID> &devlist, T cb)
 {
     static constexpr WCHAR primary_desc[] = L"Primary Sound Driver";
 
@@ -126,7 +165,7 @@ HRESULT enumerate_mmdev(EDataFlow flow, T cb)
         IID_IMMDeviceEnumerator, ds::out_ptr(devenum))};
     if(FAILED(hr))
     {
-        ERR("CoCreateInstance failed: %08lx\n", hr);
+        ERR("enumerate_mmdev CoCreateInstance failed: %08lx\n", hr);
         return hr;
     }
 
@@ -134,7 +173,7 @@ HRESULT enumerate_mmdev(EDataFlow flow, T cb)
     hr = devenum->EnumAudioEndpoints(flow, DEVICE_STATE_ACTIVE, ds::out_ptr(coll));
     if(FAILED(hr))
     {
-        WARN("EnumAudioEndpoints failed: %08lx\n", hr);
+        WARN("enumerate_mmdev IMMDeviceEnumerator::EnumAudioEndpoints failed: %08lx\n", hr);
         return DS_OK;
     }
 
@@ -142,15 +181,74 @@ HRESULT enumerate_mmdev(EDataFlow flow, T cb)
     hr = coll->GetCount(&count);
     if(FAILED(hr))
     {
-        WARN("GetCount failed: %08lx\n", hr);
+        WARN("enumerate_mmdev IMMDeviceCollection::GetCount failed: %08lx\n", hr);
         return DS_OK;
     }
 
     if(count == 0)
         return DS_OK;
 
+    std::deque<GUID>{}.swap(devlist);
+
     TRACE("enumerate_mmdev Calling back with NULL (%ls)\n", primary_desc);
     bool keep_going{cb(nullptr, primary_desc, L"")};
+
+    auto send_device = [flow,&devlist,&cb,&keep_going](IMMDevice *device)
+    {
+        ComPtr<IPropertyStore> ps;
+        HRESULT hr{device->OpenPropertyStore(STGM_READ, ds::out_ptr(ps))};
+        if(FAILED(hr))
+        {
+            WARN("send_device IMMDevice::OpenPropertyStore failed: %08lx\n", hr);
+            return;
+        }
+
+        PropVariant pv;
+        hr = ps->GetValue(PKEY_AudioEndpoint_GUID, pv.get());
+        if(FAILED(hr) || pv->vt != VT_LPWSTR)
+        {
+            WARN("send_device IPropertyStore::GetValue(GUID) failed: %08lx\n", hr);
+            return;
+        }
+
+        GUID guid;
+        CLSIDFromString(pv->pwszVal, &guid);
+        if(!devlist.empty() && devlist[0] == guid)
+            return;
+
+        devlist.emplace_back(guid);
+        if(!keep_going) return;
+
+        pv.clear();
+        hr = ps->GetValue(reinterpret_cast<const PROPERTYKEY&>(DEVPKEY_Device_FriendlyName),
+            pv.get());
+        if(FAILED(hr))
+        {
+            WARN("send_device IPropertyStore::GetValue(FriendlyName) failed: %08lx\n", hr);
+            return;
+        }
+
+        TRACE("send_device Calling back with %s - %ls\n", GuidPrinter{devlist.back()}.c_str(),
+            pv->pwszVal);
+        keep_going = cb(&devlist.back(), pv->pwszVal, L"dsoal-aldrv.dll");
+    };
+
+    ComPtr<IMMDevice> device;
+    hr = devenum->GetDefaultAudioEndpoint(flow, eMultimedia, ds::out_ptr(device));
+    if(SUCCEEDED(hr))
+        send_device(device.get());
+
+    for(UINT i{0};i < count;++i)
+    {
+        hr = coll->Item(i, ds::out_ptr(device));
+        if(FAILED(hr))
+        {
+            WARN("enumerate_mmdev IMMDeviceCollection::Item failed: %08lx\n", hr);
+            continue;
+        }
+
+        send_device(device.get());
+    }
 
     return keep_going ? S_OK : S_FALSE;
 }
@@ -179,7 +277,8 @@ HRESULT WINAPI DSOAL_DirectSoundEnumerateA(LPDSENUMCALLBACKA callback, void *use
         return callback(guid, descA.get(), modA, userPtr) != FALSE;
     };
 
-    return enumerate_mmdev(eRender, do_enum);
+    std::lock_guard listlock{gDeviceListMutex};
+    return enumerate_mmdev(eRender, gPlaybackDevices, do_enum);
 }
 HRESULT WINAPI DSOAL_DirectSoundEnumerateW(LPDSENUMCALLBACKW callback, void *userPtr)
 {
@@ -188,7 +287,8 @@ HRESULT WINAPI DSOAL_DirectSoundEnumerateW(LPDSENUMCALLBACKW callback, void *use
     auto do_enum = [callback,userPtr](GUID *guid, const WCHAR *drvname, const WCHAR *devname)
     { return callback(guid, drvname, devname, userPtr) != FALSE; };
 
-    return enumerate_mmdev(eRender, do_enum);
+    std::lock_guard listlock{gDeviceListMutex};
+    return enumerate_mmdev(eRender, gPlaybackDevices, do_enum);
 }
 
 HRESULT WINAPI DSOAL_DirectSoundCaptureEnumerateA(LPDSENUMCALLBACKA callback, void *userPtr)
@@ -211,7 +311,8 @@ HRESULT WINAPI DSOAL_DirectSoundCaptureEnumerateA(LPDSENUMCALLBACKA callback, vo
         return callback(guid, descA.get(), modA, userPtr) != FALSE;
     };
 
-    return enumerate_mmdev(eCapture, do_enum);
+    std::lock_guard listlock{gDeviceListMutex};
+    return enumerate_mmdev(eCapture, gCaptureDevices, do_enum);
 }
 HRESULT WINAPI DSOAL_DirectSoundCaptureEnumerateW(LPDSENUMCALLBACKW callback, void *userPtr)
 {
@@ -220,7 +321,8 @@ HRESULT WINAPI DSOAL_DirectSoundCaptureEnumerateW(LPDSENUMCALLBACKW callback, vo
     auto do_enum = [callback,userPtr](GUID *guid, const WCHAR *drvname, const WCHAR *devname)
     { return callback(guid, drvname, devname, userPtr) != FALSE; };
 
-    return enumerate_mmdev(eCapture, do_enum);
+    std::lock_guard listlock{gDeviceListMutex};
+    return enumerate_mmdev(eCapture, gCaptureDevices, do_enum);
 }
 
 
