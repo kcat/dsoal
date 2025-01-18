4b7373edaeec66d80770f5c4985a0b20785f2f46
Chris Robinson <chris.kcat@gmail.com>
2025-01-18 13:27:17 -0800

    Implement DSFullDuplex
    Though it won't actually work until DSCapture is more completely implemented.

--- src/fullduplex.cpp
+++ src/fullduplex.cpp
@@ -1,5 +1,7 @@
 #include "fullduplex.h"
 
+#include "capture.h"
+#include "dsoundoal.h"
 #include "guidprinter.h"
 #include "logging.h"
 
@@ -22,21 +24,33 @@ ComPtr<DSFullDuplex> DSFullDuplex::Create()
 
 
 #define PREFIX CLASS_PREFIX "QueryInterface "
-HRESULT STDMETHODCALLTYPE DSFullDuplex::QueryInterface(REFIID riid, void** ppvObject) noexcept
+HRESULT STDMETHODCALLTYPE DSFullDuplex::QueryInterface(REFIID riid, void **ppvObject) noexcept
 {
     DEBUG(PREFIX "({})->({}, {})", voidp{this}, IidPrinter{riid}.c_str(), voidp{ppvObject});
 
     *ppvObject = nullptr;
     if(riid == IID_IUnknown)
     {
-        AddRef();
-        *ppvObject = as<IUnknown*>();
+        mUnknownIface.AddRef();
+        *ppvObject = mUnknownIface.as<IUnknown*>();
         return S_OK;
     }
     if(riid == IID_IDirectSoundFullDuplex)
     {
         AddRef();
         *ppvObject = as<IDirectSoundFullDuplex*>();
+        return S_OK;
+    }
+    if(riid == IID_IDirectSound8)
+    {
+        mDS8Iface.AddRef();
+        *ppvObject = mDS8Iface.as<IDirectSound8*>();
+        return S_OK;
+    }
+    if(riid == IID_IDirectSoundCapture)
+    {
+        mDSCIface.AddRef();
+        *ppvObject = mDSCIface.as<IDirectSoundCapture*>();
         return S_OK;
     }
 
@@ -70,6 +84,191 @@ HRESULT STDMETHODCALLTYPE DSFullDuplex::Initialize(const GUID *captureGuid, cons
     DEBUG(PREFIX "({})->({}, {}, {}, {}, {}, {}, {}, {})", voidp{this},
         DevidPrinter{captureGuid}.c_str(), DevidPrinter{renderGuid}.c_str(), cvoidp{dscBufferDesc},
         cvoidp{dsBufferDesc}, voidp{hwnd}, level, voidp{dsCaptureBuffer8}, voidp{dsBuffer8});
-    return E_NOTIMPL;
+
+    if(dsCaptureBuffer8) *dsCaptureBuffer8 = nullptr;
+    if(dsBuffer8) *dsBuffer8 = nullptr;
+
+    if(!dsCaptureBuffer8 || !dsBuffer8)
+    {
+        WARN(PREFIX "Null output pointers");
+        return DSERR_INVALIDPARAM;
+    }
+
+    if(mDS8Handle || mDSCHandle)
+    {
+        WARN(PREFIX "Already initialized");
+        return DSERR_ALREADYINITIALIZED;
+    }
+
+    try {
+        mDS8Handle = DSound8OAL::Create(true);
+        if(auto hr = mDS8Handle->Initialize(renderGuid); FAILED(hr))
+        {
+            mDS8Handle = nullptr;
+            return hr;
+        }
+    }
+    catch(std::exception &e) {
+        ERR(PREFIX "Exception creating IDirectSound8: {}", e.what());
+        mDS8Handle = nullptr;
+        return E_FAIL;
+    }
+
+    mDS8Handle->SetCooperativeLevel(hwnd, level);
+
+    auto dsbuf = ComPtr<IDirectSoundBuffer>{};
+    if(auto hr = mDS8Handle->CreateSoundBuffer(dsBufferDesc, ds::out_ptr(dsbuf), nullptr);
+        FAILED(hr))
+    {
+        mDS8Handle = nullptr;
+        return hr;
+    }
+
+    try {
+        mDSCHandle = DSCapture::Create(true);
+        if(auto hr = mDSCHandle->Initialize(captureGuid); FAILED(hr))
+        {
+            mDS8Handle = nullptr;
+            mDSCHandle = nullptr;
+            return hr;
+        }
+    }
+    catch(std::exception &e) {
+        ERR(PREFIX "Exception creating IDirectSoundCapture8: {}", e.what());
+        mDS8Handle = nullptr;
+        mDSCHandle = nullptr;
+        return E_FAIL;
+    }
+
+    auto dscbuf = ComPtr<IDirectSoundCaptureBuffer>{};
+    if(auto hr = mDSCHandle->CreateCaptureBuffer(dscBufferDesc, ds::out_ptr(dscbuf), nullptr);
+        FAILED(hr))
+    {
+        mDS8Handle = nullptr;
+        mDSCHandle = nullptr;
+        return hr;
+    }
+
+    if(auto hr = dsbuf->QueryInterface(IID_IDirectSoundBuffer8,
+        reinterpret_cast<void**>(dsBuffer8)); FAILED(hr))
+        return hr;
+    if(auto hr = dscbuf->QueryInterface(IID_IDirectSoundCaptureBuffer8,
+        reinterpret_cast<void**>(dsCaptureBuffer8)); FAILED(hr))
+    {
+        (*dsBuffer8)->Release();
+        *dsBuffer8 = nullptr;
+        return hr;
+    }
+
+    return DS_OK;
 }
 #undef PREFIX
+#undef CLASS_PREFIX
+
+#define CLASS_PREFIX "DSFullDuplex::DS8::"
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::QueryInterface(REFIID riid, void **ppvObject) noexcept
+{ return impl_from_base()->QueryInterface(riid, ppvObject); }
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::DS8::AddRef() noexcept
+{
+    auto self = impl_from_base();
+    self->mTotalRef.fetch_add(1u, std::memory_order_relaxed);
+    const auto ret = self->mDS8Ref.fetch_add(1u, std::memory_order_relaxed) + 1;
+    DEBUG(CLASS_PREFIX "AddRef ({}) ref {}", voidp{this}, ret);
+    return ret;
+}
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::DS8::Release() noexcept
+{
+    auto self = impl_from_base();
+    const auto ret = self->mDS8Ref.fetch_sub(1u, std::memory_order_relaxed) - 1;
+    DEBUG(CLASS_PREFIX "Release ({}) ref {}", voidp{this}, ret);
+    if(self->mTotalRef.fetch_sub(1u, std::memory_order_relaxed) == 1u) UNLIKELY
+        delete self;
+    return ret;
+}
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::CreateSoundBuffer(const DSBUFFERDESC *bufferDesc, IDirectSoundBuffer **dsBuffer, IUnknown *outer) noexcept
+{ return impl_from_base()->mDS8Handle->CreateSoundBuffer(bufferDesc, dsBuffer, outer); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::GetCaps(DSCAPS *dsCaps) noexcept
+{ return impl_from_base()->mDS8Handle->GetCaps(dsCaps); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::DuplicateSoundBuffer(IDirectSoundBuffer *original, IDirectSoundBuffer **duplicate) noexcept
+{ return impl_from_base()->mDS8Handle->DuplicateSoundBuffer(original, duplicate); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::SetCooperativeLevel(HWND hwnd, DWORD level) noexcept
+{ return impl_from_base()->mDS8Handle->SetCooperativeLevel(hwnd, level); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::Compact() noexcept
+{ return impl_from_base()->mDS8Handle->Compact(); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::GetSpeakerConfig(DWORD *speakerConfig) noexcept
+{ return impl_from_base()->mDS8Handle->GetSpeakerConfig(speakerConfig); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::SetSpeakerConfig(DWORD speakerConfig) noexcept
+{ return impl_from_base()->mDS8Handle->SetSpeakerConfig(speakerConfig); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::Initialize(const GUID *deviceId) noexcept
+{ return impl_from_base()->mDS8Handle->Initialize(deviceId); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DS8::VerifyCertification(DWORD *certified) noexcept
+{ return impl_from_base()->mDS8Handle->VerifyCertification(certified); }
+#undef CLASS_PREFIX
+
+#define CLASS_PREFIX "DSFullDuplex::DSC::"
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DSC::QueryInterface(REFIID riid, void **ppvObject) noexcept
+{ return impl_from_base()->QueryInterface(riid, ppvObject); }
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::DSC::AddRef() noexcept
+{
+    auto self = impl_from_base();
+    self->mTotalRef.fetch_add(1u, std::memory_order_relaxed);
+    const auto ret = self->mDSCRef.fetch_add(1u, std::memory_order_relaxed) + 1;
+    DEBUG(CLASS_PREFIX "AddRef ({}) ref {}", voidp{this}, ret);
+    return ret;
+}
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::DSC::Release() noexcept
+{
+    auto self = impl_from_base();
+    const auto ret = self->mDSCRef.fetch_sub(1u, std::memory_order_relaxed) - 1;
+    DEBUG(CLASS_PREFIX "Release ({}) ref {}", voidp{this}, ret);
+    if(self->mTotalRef.fetch_sub(1u, std::memory_order_relaxed) == 1u) UNLIKELY
+        delete self;
+    return ret;
+}
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DSC::CreateCaptureBuffer(const DSCBUFFERDESC *dscBufferDesc, IDirectSoundCaptureBuffer **dsCaptureBuffer, IUnknown *unk) noexcept
+{ return impl_from_base()->mDSCHandle->CreateCaptureBuffer(dscBufferDesc, dsCaptureBuffer, unk); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DSC::GetCaps(DSCCAPS *dscCaps) noexcept
+{ return impl_from_base()->mDSCHandle->GetCaps(dscCaps); }
+
+HRESULT STDMETHODCALLTYPE DSFullDuplex::DSC::Initialize(const GUID *guid) noexcept
+{ return impl_from_base()->mDSCHandle->Initialize(guid); }
+#undef CLASS_PREFIX
+
+#define CLASS_PREFIX "DSFullDuplex::Unknown::"
+HRESULT STDMETHODCALLTYPE DSFullDuplex::Unknown::QueryInterface(REFIID riid, void **ppvObject) noexcept
+{ return impl_from_base()->QueryInterface(riid, ppvObject); }
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::Unknown::AddRef() noexcept
+{
+    auto self = impl_from_base();
+    self->mTotalRef.fetch_add(1u, std::memory_order_relaxed);
+    const auto ret = self->mUnkRef.fetch_add(1u, std::memory_order_relaxed) + 1;
+    DEBUG(CLASS_PREFIX "AddRef ({}) ref {}", voidp{this}, ret);
+    return ret;
+}
+
+ULONG STDMETHODCALLTYPE DSFullDuplex::Unknown::Release() noexcept
+{
+    auto self = impl_from_base();
+    const auto ret = self->mUnkRef.fetch_sub(1u, std::memory_order_relaxed) - 1;
+    DEBUG(CLASS_PREFIX "Release ({}) ref {}", voidp{this}, ret);
+    if(self->mTotalRef.fetch_sub(1u, std::memory_order_relaxed) == 1u) UNLIKELY
+            delete self;
+    return ret;
+}
+#undef CLASS_PREFIX
--- src/fullduplex.h
+++ src/fullduplex.h
@@ -7,13 +7,116 @@
 
 #include "comptr.h"
 
+class DSound8OAL;
+class DSCapture;
 
 class DSFullDuplex final : IDirectSoundFullDuplex {
-    std::atomic<ULONG> mTotalRef{1u}, mFdRef{1u};
+    DSFullDuplex();
+
+    class Unknown final : IUnknown {
+        DSFullDuplex *impl_from_base() noexcept
+        {
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic push")
+            _Pragma("GCC diagnostic ignored \"-Wcast-align\"")
+#endif
+            return CONTAINING_RECORD(this, DSFullDuplex, mUnknownIface);
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic pop")
+#endif
+        }
+
+    public:
+        HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject) noexcept override;
+        ULONG STDMETHODCALLTYPE AddRef() noexcept override;
+        ULONG STDMETHODCALLTYPE Release() noexcept override;
+
+        Unknown() = default;
+        Unknown(const Unknown&) = delete;
+        Unknown& operator=(const Unknown&) = delete;
+
+        template<typename T>
+        T as() noexcept { return static_cast<T>(this); }
+    };
+    Unknown mUnknownIface;
+
+    class DS8 final : IDirectSound8 {
+        DSFullDuplex *impl_from_base() noexcept
+        {
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic push")
+            _Pragma("GCC diagnostic ignored \"-Wcast-align\"")
+#endif
+            return CONTAINING_RECORD(this, DSFullDuplex, mDS8Iface);
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic pop")
+#endif
+        }
+
+    public:
+        /*** IUnknown methods ***/
+        HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject) noexcept override;
+        ULONG STDMETHODCALLTYPE AddRef() noexcept override;
+        ULONG STDMETHODCALLTYPE Release() noexcept override;
+        /*** IDirectSound8 methods ***/
+        HRESULT STDMETHODCALLTYPE CreateSoundBuffer(const DSBUFFERDESC *bufferDesc, IDirectSoundBuffer **dsBuffer, IUnknown *outer) noexcept override;
+        HRESULT STDMETHODCALLTYPE GetCaps(DSCAPS *caps) noexcept override;
+        HRESULT STDMETHODCALLTYPE DuplicateSoundBuffer(IDirectSoundBuffer *original, IDirectSoundBuffer **duplicate) noexcept override;
+        HRESULT STDMETHODCALLTYPE SetCooperativeLevel(HWND hwnd, DWORD level) noexcept override;
+        HRESULT STDMETHODCALLTYPE Compact() noexcept override;
+        HRESULT STDMETHODCALLTYPE GetSpeakerConfig(DWORD *speakerConfig) noexcept override;
+        HRESULT STDMETHODCALLTYPE SetSpeakerConfig(DWORD speakerConfig) noexcept override;
+        HRESULT STDMETHODCALLTYPE Initialize(const GUID *deviceId) noexcept override;
+        HRESULT STDMETHODCALLTYPE VerifyCertification(DWORD *certified) noexcept override;
+
+        DS8() = default;
+        DS8(const DS8&) = delete;
+        DS8& operator=(const DS8&) = delete;
+
+        template<typename T>
+        T as() noexcept { return static_cast<T>(this); }
+    };
+    DS8 mDS8Iface;
+
+    class DSC final : IDirectSoundCapture {
+        DSFullDuplex *impl_from_base() noexcept
+        {
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wcast-align\"")
+#endif
+            return CONTAINING_RECORD(this, DSFullDuplex, mDSCIface);
+#ifdef __GNUC__
+            _Pragma("GCC diagnostic pop")
+#endif
+        }
+
+    public:
+        HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject) noexcept override;
+        ULONG STDMETHODCALLTYPE AddRef() noexcept override;
+        ULONG STDMETHODCALLTYPE Release() noexcept override;
+        HRESULT STDMETHODCALLTYPE CreateCaptureBuffer(const DSCBUFFERDESC *dscBufferDesc, IDirectSoundCaptureBuffer **dsCaptureBuffer, IUnknown *unk) noexcept override;
+        HRESULT STDMETHODCALLTYPE GetCaps(DSCCAPS *dscCaps) noexcept override;
+        HRESULT STDMETHODCALLTYPE Initialize(const GUID *guid) noexcept override;
+
+        DSC() = default;
+        DSC(const DSC&) = delete;
+        DSC& operator=(const DSC&) = delete;
+
+        template<typename T>
+        T as() noexcept { return static_cast<T>(this); }
+    };
+    DSC mDSCIface;
+
+    std::atomic<ULONG> mTotalRef{1u}, mFdRef{1u}, mDS8Ref{0u}, mDSCRef{0u}, mUnkRef{0u};
+
+    ComPtr<DSound8OAL> mDS8Handle;
+    ComPtr<DSCapture> mDSCHandle;
 
 public:
-    DSFullDuplex();
     ~DSFullDuplex();
+
+    DSFullDuplex(const DSFullDuplex&) = delete;
+    DSFullDuplex& operator=(const DSFullDuplex&) = delete;
 
     HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppvObject) noexcept override;
     ULONG STDMETHODCALLTYPE AddRef() noexcept override;
